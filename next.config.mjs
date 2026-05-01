249ed1e0a41e2dc15ca78fe33aeb4dbc22e87a85
Rohit0035 <75827103+Rohit0035@users.noreply.github.com>
2026-05-01 20:13:01 +0530

    new changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,26 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   experimental: {
+//     appDir: true,
+//   },
+//   output: "export", 
+//   images: {
+//     unoptimized: true, 
+//   }
+// };
+
+// export default nextConfig;
+
+
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
+    appDir: true, // This is fine if you're using the app directory (for Next.js 13+)
   },
-  output: "export", 
   images: {
-    unoptimized: true, 
-  }
+    unoptimized: true, // Keep this if you don't want Next.js to optimize images
+  },
 };
 
 export default nextConfig;
